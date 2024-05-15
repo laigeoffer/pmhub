1872d2f1d4afce4de1ee4afb0d2ab2c45d8c411c
canghe <2689458656@qq.com>
2024-05-15 23:53:13 +0800

    refactor/增加配置

--- pmhub-base/pmhub-base-core/src/main/java/com/laigeoffer/pmhub/base/core/config/ResourcesConfig.java
+++ pmhub-base/pmhub-base-core/src/main/java/com/laigeoffer/pmhub/base/core/config/ResourcesConfig.java
@@ -1,24 +1,24 @@
-package com.laigeoffer.pmhub.base.core.config;
-
-import com.laigeoffer.pmhub.base.core.constant.Constants;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * 通用配置
- *
- * @author canghe
- */
-@Configuration
-public class ResourcesConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        /** 本地文件上传路径 */
-        registry.addResourceHandler(Constants.RESOURCE_PREFIX + "/**")
-                .addResourceLocations("file:" + PmhubConfig.getProfile() + "/");
-    }
-
-
-}
+//package com.laigeoffer.pmhub.base.core.config;
+//
+//import com.laigeoffer.pmhub.base.core.constant.Constants;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 通用配置
+// *
+// * @author canghe
+// */
+//@Configuration
+//public class ResourcesConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        /** 本地文件上传路径 */
+//        registry.addResourceHandler(Constants.RESOURCE_PREFIX + "/**")
+//                .addResourceLocations("file:" + PmhubConfig.getProfile() + "/");
+//    }
+//
+//
+//}
