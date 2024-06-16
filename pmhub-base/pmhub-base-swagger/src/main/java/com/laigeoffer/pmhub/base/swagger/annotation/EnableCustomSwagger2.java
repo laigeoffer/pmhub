329fbd04eb16bb00756ecef08619f7910fc6ab66
canghe <2689458656@qq.com>
2024-06-16 22:45:27 +0800

    feat/添加单个服务swagger

---
+++ pmhub-base/pmhub-base-swagger/src/main/java/com/laigeoffer/pmhub/base/swagger/annotation/EnableCustomSwagger2.java
@@ -0,0 +1,16 @@
+package com.laigeoffer.pmhub.base.swagger.annotation;
+
+import com.laigeoffer.pmhub.base.swagger.config.SwaggerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({ SwaggerAutoConfiguration.class })
+public @interface EnableCustomSwagger2
+{
+
+}
