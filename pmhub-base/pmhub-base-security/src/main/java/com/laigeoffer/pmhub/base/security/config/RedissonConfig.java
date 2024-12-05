0529dfb6da20153be645d9da8b73aa0ac34f60fa
苍何 <2689458656@qq.com>
2024-12-05 23:30:12 +0800

    bugfix/修改RedissonConfig默认配置从nacos取

--- pmhub-base/pmhub-base-security/src/main/java/com/laigeoffer/pmhub/base/security/config/RedissonConfig.java
+++ pmhub-base/pmhub-base-security/src/main/java/com/laigeoffer/pmhub/base/security/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.laigeoffer.pmhub.base.security.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +14,20 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password:}") // 如果没有密码，默认值为空
+    private String redisPassword;
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379")
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setPassword(redisPassword.isEmpty() ? null : redisPassword)
                 .setDatabase(0);
 
         return Redisson.create(config);
